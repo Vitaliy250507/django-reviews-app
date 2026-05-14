e2920c6e074b9cf80fca67c0c65ac17316c771c9
Vitaliy250507 <vataliy.k2312@gmail.com>
2026-05-14 15:30:16 +0300

    Added restaurant detail page with reviews

--- Review/my_site/review/urls.py
+++ Review/my_site/review/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.BaseView.as_view(), name='review-home'),
-    path('restaurants', views.RestaurantListView.as_view(), name = 'restaurants')
+    path('restaurants', views.RestaurantListView.as_view(), name = 'restaurants'),
+    path('restaurants/<int:pk>/', views.RestaurantDetailView.as_view(), name = 'restaurant-detail')
 ]
--- Review/my_site/review/views.py
+++ Review/my_site/review/views.py
@@ -4,6 +4,7 @@ from django.views.generic.list import ListView
 from django.views.generic import View
 from .models import Restaurant
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
 # Create your views here.
 
 class RestaurantListView(ListView):
@@ -14,4 +15,14 @@ class RestaurantListView(ListView):
 class BaseView(View):
     def get(self, request):
         return render(request, 'base.html')
+    
+
+class RestaurantDetailView(DetailView):
+    template_name = "reviews/restaurant.html"
+    model = Restaurant
+    context_object_name = "restaurant"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["allreviews"] = self.object.reviews.all
+        return context
     
