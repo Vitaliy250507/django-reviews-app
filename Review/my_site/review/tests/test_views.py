8ae49161c9e59f589af98d676a4e7e6044cbbaf7
Vitaliy250507 <vataliy.k2312@gmail.com>
2026-05-25 15:27:15 +0300

    RestaurantDetailView tested

--- Review/my_site/review/tests/test_views.py
+++ Review/my_site/review/tests/test_views.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from ..models import Restaurant, Review, Profile
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.contrib.messages import get_messages
 
 class RestaurantListViewTests(TestCase):
     def setUp(self):
@@ -13,7 +14,8 @@ class RestaurantListViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
     
     def test_restaurant_list_no_filter(self):
-        response = self.client.get(reverse('restaurants'), 200)
+        response = self.client.get(reverse('restaurants')) 
+        self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Pizza Hut')
         self.assertContains(response, 'Sushi Master')
 
@@ -28,4 +30,41 @@ class RestaurantListViewTests(TestCase):
         response = self.client.get(reverse('restaurants'), {'top':'true'})
         self.assertContains(response, 'Pizza Hut')
         self.assertNotContains(response, 'Sushi Master')
-        
+
+
+class RestaurantDetailViewTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="vitaliy", password="123")
+        self.restaurant = Restaurant.objects.create(name="Пузата Хата", address="Київ")
+        self.url = reverse('restaurant-detail', kwargs={'pk': self.restaurant.pk})
+
+    def test_get_context_data(self):
+        self.client.login(username="vitaliy", password='123')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('form', response.context) 
+        self.assertIn('allreviews', response.context)
+    
+    def test_post_review_already_exists(self):
+        self.client.login(username='vitaliy', password='123')
+        Review.objects.create(restaurant=self.restaurant, user=self.user, rating=5, text="Один")
+        response = self.client.post(self.url, {'rating':'4', 'text': 'Другий'})
+        self.assertRedirects(response, self.url)
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(str(messages[0]), "Ви вже залишили відгук для цього закладу. Дякуємо!")
+
+    def test_post_review_success(self):
+        self.client.login(username="vitaliy", password="123")
+        data = {
+            'rating': 5,
+            'text': 'Неймовірно смачно'
+        }
+        response = self.client.post(self.url, data)
+        self.assertRedirects(response, self.url)
+        self.assertTrue(Review.objects.filter(text='Неймовірно смачно').exists())
+
+    def test_post_review_invalid(self):
+        self.client.login(username="vitaliy", password="123")
+        response = self.client.post(self.url, {'rating': '', 'text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(response.context['form'].is_valid())
